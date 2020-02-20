1c774d467d004a397778069a440a7976941dff08
JamesProgram <jas_bailey@yahoo.com>
2020-02-20 12:44:26 -0700

    made changes to structure

---
+++ starwars/main.js
@@ -0,0 +1,9 @@
+console.log('Hi there welcome to my page')
+
+//console.log(document.querySelector(".greeting"))
+
+const greetingDiv = document.querySelector(".greeting")
+
+greetingDiv.textContent = "I just intserted text into a Dom"
+
+console.log(greetingDiv.textContent)
