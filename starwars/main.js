420a1dd9c0b632e0b1d9330c878e7acaa72a620c
JamesProgram <jas_bailey@yahoo.com>
2020-02-25 10:04:39 -0700

    import javascript api

--- starwars/main.js
+++ starwars/main.js
@@ -1,9 +1,15 @@
+import { films } from '../data/films.js'
+
 console.log('Hi there welcome to my page')
 
 //console.log(document.querySelector(".greeting"))
 
 const greetingDiv = document.querySelector(".greeting")
 
-greetingDiv.textContent = "I just intserted text into a Dom"
+greetingDiv.textContent = "I just intserted text into a DOM element using my mad JavaScript skills!"
 
 console.log(greetingDiv.textContent)
+
+console.log(films)
+
+greetingDiv.textContent = films [0].opening_crawl
