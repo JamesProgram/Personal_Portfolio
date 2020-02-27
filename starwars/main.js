ce18d2c2e797ac7e0639332265de85dcd346a8c6
JamesProgram <jas_bailey@yahoo.com>
2020-02-26 20:16:56 -0700

    added images to star wars page and added clicks

--- starwars/main.js
+++ starwars/main.js
@@ -13,15 +13,21 @@ let counter = 1
 people.forEach(person => {
    let listItem = document.createElement("li")
    listItem.textContent = person.name
-
    castList.appendChild(listItem)
 
+   let anchorWrap = document.createElement("a")
+   anchorWrap.href = "#"
+
    let imageItem = document.createElement("img")
-   imageItem.src = ""
-   greetingDiv.appendChild(imageItem)
+   imageItem.src = `https://starwars-visualguide.com/assets/img/characters/${counter}.jpg`
+   
    // add some way to handle user clicks on the image
-   imageItem.addEventListener("click", () => console.log("Click happend"))
-   counter++
-});
+   imageItem.addEventListener("click", (event) => {
+   console.log(event)
+})
+anchorWrap.appendChild(imageItem)
+greetingDiv.appendChild(anchorWrap)
+counter++
+})
 
 greetingDiv.appendChild(castList)
