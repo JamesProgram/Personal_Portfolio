79141e2b22551be0180ca3f3a007c21f1cfbb9f8
JamesProgram <jas_bailey@yahoo.com>
2020-03-05 12:45:06 -0700

    added starships api and js

--- starwars/main.js
+++ starwars/main.js
@@ -2,7 +2,7 @@ import { films } from '../data/films.js'
 import { people } from '../data/people.js'
 import { starships } from '../data/starships.js'
 
-const greetingDiv = document.querySelector(".greeting")
+const gallery = document.querySelector(".gallery")
 const maleButton = document.querySelector("#maleButton")
 const femaleButton = document.querySelector("#femaleButton")
 const otherButton = document.querySelector("#otherButton")
@@ -29,8 +29,6 @@ maleButton.addEventListener("click", event => {
    populateDOM(otherCharacters)
   })
 
-//"url": "https://swapi.co/api/people.10/"
-
 function getCharNumber(url) {
 let end = url.lastIndexOf('/')
 let start = end - 2
@@ -40,10 +38,17 @@ start++
 return url.slice(start, end)
 }
 
+function removeChildren(element) {
+   while (element.firstChild) {
+     element.removeChild(element.firstChild);
+   }
+}
+
 //getCharNumber("https://swapi.co/api/people.1/")
 
 function populateDOM(characters)
-{characters.forEach(person => {
+{removeChildren(gallery)
+   characters.forEach(person => {
    // need to extract the number from the person.url property
    let charNum = getCharNumber(person.url)
    let anchorWrap = document.createElement("a")
@@ -65,7 +70,7 @@ function populateDOM(characters)
    console.log(event)
 })
 anchorWrap.appendChild(imageItem)
-greetingDiv.appendChild(anchorWrap)
+gallery.appendChild(anchorWrap)
 
 })
 }
