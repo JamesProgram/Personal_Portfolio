b552b07a31a96e1d05f203b6cbb5481abb8629e0
JamesProgram <jas_bailey@yahoo.com>
2020-03-10 12:45:19 -0600

    added functionality on starships

--- starships/main.js
+++ starships/main.js
@@ -9,6 +9,11 @@ function populateNav(starships) {
 
    let anchorWrap = document.createElement("a")
    anchorWrap.href = "#"
+   anchorWrap.addEventListener('click', event => {
+      let shipName = event.target.textContent
+      const foundShip = starships.find(ship => shipName === shipName)
+      populateShipView.log(foundShip)
+   })
 
    let listItem = document.createElement('li')
    listItem.textContent = starship.name
@@ -18,6 +23,22 @@ navList.appendChild(anchorWrap)
 nav.appendChild(navList)
 
 })
+}
+
+function getCharNumber(url) {
+   let end = url.lastIndexOf('/')
+   let start = end - 2
+   if(url.charAt(start) === '/') {
+   start++
+   }
+   return url.slice(start, end)
+   }
+
+function populateShipView(shipData) {
+   let shipNum = getCharNumber(shipData.url)
+   let shipImage = document.createElement("img")
+   shipImage.src = `https://starwars-visualguide.com/assets/img/characters/${shipNum}.jpg`
+
 }
 
 populateNav(starships)
