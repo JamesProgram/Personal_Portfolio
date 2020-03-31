be87711a267e5fd01921779c42cc255f67d3199b
JamesProgram <jas_bailey@yahoo.com>
2020-03-30 20:08:24 -0600

    map senators page

--- congress/index.js
+++ congress/index.js
@@ -5,8 +5,32 @@ const main = document.querySelector('main')
 
 function populateSenatorDiv() {
     senators.forEach( senator => {
-        console.log(`${senator.first_name} ${senator.last_name}`)
+        let senatorName = `${senator.first_name} ${senator.last_name}`
+        let nameParagraph = document.createElement('p')
+        nameParagraph.textContent = senatorName
+        senatorDiv.appendChild(nameParagraph)
     })
+    main.appendChild(senatorDiv)
 }
+
+const filterSenators = (prop, value) => {
+   return senators.filter(senator => {
+       return senator[prop] === value
+   })
+}
+
+/* console.log(filterSenators('party', 'R'))
+console.log(filterSenators('party', 'D'))
+console.log(filterSenators('party', 'ID')) */
+
+const senatorNames = senators.map(senator => {
+    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+    return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`
+    }
+})
+
+console.log(senatorNames)
 
 populateSenatorDiv()
