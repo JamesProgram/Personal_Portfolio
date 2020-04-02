b0c4c906d459f07253a8e34cdac8ed2b21ebf7df
JamesProgram <jas_bailey@yahoo.com>
2020-04-01 18:38:35 -0600

    cleaned up js code

--- congress/index.js
+++ congress/index.js
@@ -2,16 +2,23 @@ import { senators } from '../data/senators.js'
 
 const senatorDiv = document.querySelector('.senators')
 
-function populateSenatorDiv() {
-    senators.forEach( senator => {
-        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+const senatorNames = senators.map(senator => {
+    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+    return {
+        id: senator.id,
+        name: `${senator.first_name}${middleName}${senator.last_name}`,
+        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
+    }
+})
 
+function populateSenatorDiv(justSenatorNames) {
+    justSenatorNames.forEach( senator => {
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
             
-        figImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
-        figCaption.textContent = `${senator.first_name}${middleName}${senator.last_name}`
+        figImg.src = senator.imgURL
+        figCaption.textContent = senator.name
 
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
@@ -27,20 +34,4 @@ const filterSenators = (prop, value) => {
    })
 }
 
-/* console.log(filterSenators('party', 'R'))
-console.log(filterSenators('party', 'D'))
-console.log(filterSenators('party', 'ID')) */
-
-const senatorNames = senators.map(senator => {
-    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
-    return {
-        id: senator.id,
-        name: `${senator.first_name}${middleName}${senator.last_name}`,
-        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
-    }
-})
-
-//https://www.govtrack.us/static/legislator-photos/300002-200px.jpeg
-
-
-populateSenatorDiv()
+populateSenatorDiv(senatorNames)
