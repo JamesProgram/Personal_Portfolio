f7a9a0cd54859850b108f71ee973c7bb1f9d2fb9
JamesProgram <jas_bailey@yahoo.com>
2020-04-09 11:00:04 -0600

    Made changes to senator js/error on line 55

--- congress/index.js
+++ congress/index.js
@@ -1,21 +1,13 @@
 import { senators } from '../data/senators.js'
 const senatorDiv = document.querySelector('.senators')
-
 function getSimplifiedSenators(senatorArray) {
     return senatorArray.map(senator => {
-    let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
-    return {
-        id: senator.id,
-        name: `${senator.first_name}${middleName}${senator.last_name}`,
-        imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
-        seniority: parseInt(senator.seniority, 10)
-    }
-})
         let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
         return {
             id: senator.id,
             name: `${senator.first_name}${middleName}${senator.last_name}`,
             imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
+            seniority: parseInt(senator.seniority, 10),
             seniority: parseInt(senator.seniority, 10),
             missedVotesPct: senator.missed_votes_pct,
             party: senator.party,
@@ -23,29 +15,21 @@ function getSimplifiedSenators(senatorArray) {
         }
     })
 }
-
 function populateSenatorDiv(simpleSenators) {
     console.log(simpleSenators)
-    simpleSenators.forEach( senator => {
     simpleSenators.forEach(senator => {
         let senFigure = document.createElement('figure')
         let figImg = document.createElement('img')
         let figCaption = document.createElement('figcaption')
             
-
         figImg.src = senator.imgURL
         figCaption.textContent = senator.name
-
         senFigure.appendChild(figImg)
         senFigure.appendChild(figCaption)
         senatorDiv.appendChild(senFigure)
     })
 }
-
 const filterSenators = (prop, value) => {
-   return senators.filter(senator => {
-       return senator[prop] === value
-   })
     return senators.filter(senator => {
         return senator[prop] === value
     })
@@ -56,31 +40,23 @@ const democrats = filterSenators('party', 'D')
 
 const mostSeniority = getSimplifiedSenators(republicans).reduce(
     (acc, senator) => {
-    return acc.seniority > senator.seniority ? acc : senator
-    },
-   )
-   const mostSeniority = getSimplifiedSenators(senators).reduce((acc, senator) =>
-    acc.seniority > senator.seniority ? acc : senator,
-   )
-    
-   const missedVotes = getSimplifiedSenators(senators).reduce((acc, senator) =>
-    acc.missedVotesPct > senator.missedVotesPct ? acc : senator,
-   )
-    
-   let loyalArray = []
-    
-   const mostLoyal = getSimplifiedSenators(republicans).reduce((acc, senator) => {
+        return acc.seniority > senator.seniority ? acc : senator
+const mostSeniority = getSimplifiedSenators(senators).reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
+
+const missedVotes = getSimplifiedSenators(senators).reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
+
+let loyalArray = []
+
+const mostLoyal = getSimplifiedSenators(republicans).reduce((acc, senator) => {
     if (senator.loyaltyPct === 100) {
-    loyalArray.push(senator)
+        loyalArray.push(senator)
     }
-    
+}
     return acc.loyaltyPct > senator.loyaltyPct ? acc : senator
-   })
+})
 
-
-console.log(mostSeniority)
+console.log(mostSeniority),
 console.log(loyalArray)
 
 populateSenatorDiv(getSimplifiedSenators(republicans))
 populateSenatorDiv(getSimplifiedSenators(senators))
-}
